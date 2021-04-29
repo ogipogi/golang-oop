fcd40888d177f94be9dbf34c0c8a4e21d2108f59
Graziano Felline <graziano.felline@adcubum.com>
2021-04-29 21:11:00 +0200

    impl for manufacturer from vin with 9 as third digit

--- vin/vin.go
+++ vin/vin.go
@@ -1,5 +1,20 @@
 package vin
 
-func Manufacturer(vin string) string  {
+func Manufacturer(vin string) string {
+
+	if string(vin[2]) == "9" {
+		return vin[:3] + vin[11:14]
+	}
 	return vin[:3]
+
+	// Variante 2
+	/*
+		manufactorer := vin[:3]
+		if string(vin[2]) == "9" {
+			manufactorer += vin[11:14]
+		}
+		return manufactorer
+
+	*/
+
 }
