4ca667ea7d8e18199c1f33a2ae188c112e3b0dd4
Graziano Felline <graziano.felline@adcubum.com>
2021-05-06 20:23:50 +0200

    impl TestFizzBuzz_1

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -1,12 +1,9 @@
 package fizzbuzz
 
-import "fmt"
-
 // Fizzbuzz is a program that prints integer numbers from 1 to n. But for multiples of three,
 // print “Fizz” instead of the number and for the multiples of five print “Buzz”.
 // For numbers which are multiples of both three and five print “FizzBuzz”.
 
-func Fizzbuzz(n int) string  {
-	fmt.Println("fizzbuzz")
-	return ""
+func FizzBuzz(n int) string  {
+	return "1"
 }
