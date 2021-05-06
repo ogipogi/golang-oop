9d666c0b143ec9c3109dffc10cfc4af5a0d226af
Graziano Felline <graziano.felline@adcubum.com>
2021-05-06 20:23:36 +0200

    test TestFizzBuzz_1

--- fizzbuzz_test/fizzbuzz_test.go
+++ fizzbuzz_test/fizzbuzz_test.go
@@ -1,1 +1,18 @@
 package fizzbuzz_test
+
+import (
+	"golang-oop/fizzbuzz"
+	"testing"
+)
+
+func TestFizzBuzz_1(t *testing.T) {
+
+	// Act
+	result := fizzbuzz.FizzBuzz(1)
+	expected := "1"
+
+	// Assert
+	if result != expected {
+		t.Errorf("FizzBuzz(1) result: %v : expected %v", result, expected)
+	}
+}
