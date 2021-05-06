850033544752cf031b64c51ffe0a10e5f264bbfc
Graziano Felline <graziano.felline@adcubum.com>
2021-05-06 18:45:58 +0200

    fizzbuzz files created

---
+++ fizzbuzz/fizzbuzz.go
@@ -0,0 +1,5 @@
+package fizzbuzz
+
+// Fizzbuzz is a program that prints interger numbers from 1 to n. But for multiples of three,
+// print “Fizz” instead of the number and for the multiples of five print “Buzz”.
+// For numbers which are multiples of both three and five print “FizzBuzz”.
